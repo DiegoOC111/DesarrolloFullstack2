08626b2a0705df7df65557363f58f87a7b60d9ae
Carlos Eduardo Calvio Saavedra <ca.calvio@duocuc.cl>
2025-10-22 15:39:14 -0300

    agregue pruebas para productos de pages
    al aplicarlo tengo una funcion que no funciona

--- WebAppTech/appweb/src/pages/Carritos.spec.js
+++ WebAppTech/appweb/src/pages/Carritos.spec.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom"; // <-- SOLUCIÓN: Importa los matchers de extensión
+import "@testing-library/jest-dom"; 
 import React from "react";
 import Carrito from "../pages/Carrito"; 
 
